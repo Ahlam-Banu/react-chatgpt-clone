611ea24e462f32343fc1147dfd5c89b9a4a9ea4b
Ahlam-Banu <ahlambanu.univ@gmail.com>
2023-09-06 23:35:06 +0300

    Changes to App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ const App = () => {
   return (
     <div className="app">
       <section className="sidebar">
-        {/* Other content in the sidebar */}
         <button>+ New Chat</button>
-        {/* Button to toggle the sidebar */} 
+        {/* Going to add a Button next to 'New Chat' button to toggle the sidebar */}
+        <ul className="history"></ul>
+        <nav> {/* <nav> is used to semantically mark up navigation menus, which can include links to various pages or sections of the website.
+It is often placed in the header or footer of a webpage to provide navigation options for users. */}
+          <p>Made by Ahlam</p>
+        </nav>
       </section>
       <section className="main"></section>
     </div>
