e3c590b5213d8147f5c9a21b41daa7de28fb4ed4
Ahlam-Banu <ahlambanu.univ@gmail.com>
2024-01-04 01:09:03 +0200

    backend api bug fixed

---
+++ server.js
@@ -0,0 +1,31 @@
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const API_KEY = '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';
+
+app.post("/completions", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: req.body.message }],
+      max_tokens: 100,
+    })
+  }
+  try {
+    const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+    const data = await response.json()
+    res.send(data)
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error processing your request");
+  }
+})
--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
+import { useState, useEffect } from "react";
+
 const App = () => {
+  const [value, setValue] = useState(null); //to hookup the input
+  const [message, setMessage] = useState(null);
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+    try { 
+      const response = await fetch("http://localhost:8000/completions", options);
+      console.log("thsi is a test, after fetch")
+      const data = await response.json();
+      // console.log(data);
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  console.log(message)
+
   return (
     <div className="app">
       <section className="side-bar">
@@ -11,21 +39,23 @@ const App = () => {
       </section>
       <section className="main">
         <h1>AhlamGPT</h1>
-        <ul className="feed">
-
-        </ul>
+        <ul className="feed"></ul>
         <div className="bottom-section">
-          <div className="input-container"> {/* this <div> will be used to contain input elements */}
-            <input placeholder="Send a message"/>
-            <div id = "submit">➔</div>
+          <div className="input-container">
+            {/* this <div> will be used to contain input elements */}
+            <input value = {value} onChange={(e) => setValue(e.target.value)} />
+            <div id="submit" onClick={getMessages}>
+              ➔
+            </div>
           </div>
           <p className="info">
-          Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts.
+            Free Research Preview. ChatGPT may produce inaccurate information
+            about people, places, or facts.
           </p>
         </div>
       </section>
     </div>
   );
-}
+};
 
 export default App;
